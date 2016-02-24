d6877a1e4ec7b56f309c6bd1605b8e39d125517f
darksun <lujun9972@sina.com>
2016-02-24 16:30:27 +0800

    add macro auto-pause

--- auto-pause.el
+++ auto-pause.el
@@ -1,4 +1,5 @@
 ;; -*- lexical-binding: t; -*-
+(require 'cl-lib)
 (defun auto-pause--pause-process (proc)
   ""
   (when (processp proc) 
@@ -23,6 +24,7 @@
   (run-with-idle-timer delay-seconds nil (lambda ()
                                            (auto-pause-pause-process proc))))
 
+
 (defun auto-pause-mark-process (proc delay-seconds)
   (run-with-idle-timer delay-seconds nil (lambda ()
                                            (auto-pause-pause-process proc)))
@@ -38,3 +40,37 @@
                  :name advise-name)
      (unwind-protect (progn ,@body)
        (advice-remove start-process advise-name))))
+
+;; (defun auto-pause-make-pause-and-resume-functions (pause-fn resume-fn)
+;;   (let (pause-function resume-function)
+;;     (setq pause-function (lambda ()
+;;                            (apply pause-fn)
+;;                            (add-hook 'post-command-hook resume-function)))
+;;     (setq resume-function (lambda (delay-seconds)
+;;                             (apply resume-fn)
+;;                             (remove-hook 'post-command-hook resume-function)
+;;                             (run-with-idle-timer delay-seconds nil pause-function)))
+;;     (list pause-function resume-function)))
+
+(defmacro auto-pause (pause-fn resume-fn delay-seconds)
+  (let (;; (pause-fn (function pause-fn))
+        ;; (resume-fn (function resume-fn))
+        (pause-function-name (cl-gensym "auto-pause-pause-"))
+        (resume-function-name (cl-gensym "auto-pause-resume-"))
+        (abort-function-name (cl-gensym "auto-pause-abort-"))
+        (idle-timer (cl-gensym "auto-pause-idle-timer-"))
+        (delay-seconds delay-seconds))
+    `(progn
+       (defun ,pause-function-name ()
+         (funcall ,pause-fn )
+         (add-hook 'post-command-hook #',resume-function-name))
+       (defun ,resume-function-name ()
+         (funcall ,resume-fn )
+         (remove-hook 'post-command-hook #',resume-function-name)) 
+       (setq ,idle-timer (run-with-idle-timer ,delay-seconds t #',pause-function-name))
+       (defun ,abort-function-name ()
+         (cancel-timer ,idle-timer)
+         (unintern ',idle-timer nil)
+         (unintern ',pause-function-name nil)
+         (unintern ',resume-function-name nil)
+         (unintern ',abort-function-name nil)))))
