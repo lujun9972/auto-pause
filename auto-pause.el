61aa239ea7b558ef306b871c1c40ecf5f0b0c37b
darksun <lujun9972@sina.com>
2016-02-26 16:20:26 +0800

    not finished

--- auto-pause.el
+++ auto-pause.el
@@ -3,9 +3,7 @@
 
 (defmacro auto-pause (pause-fn resume-fn delay-seconds)
   (declare (debug t) (indent defun))
-  (let ( ;; (pause-fn (function pause-fn))
-        ;; (resume-fn (function resume-fn))
-        (pause-function-name (cl-gensym "auto-pause-pause-"))
+  (let ((pause-function-name (cl-gensym "auto-pause-pause-"))
         (resume-function-name (cl-gensym "auto-pause-resume-"))
         (abort-function-name (cl-gensym "auto-pause-abort-"))
         (idle-timer (cl-gensym "auto-pause-idle-timer-"))
@@ -35,6 +33,22 @@
   (when (processp proc)
     (signal-process proc 'SIGCONT)))
 
+(defun auto-pause-process-p (proc)
+  "Return t if PROC is a auto-pause process"
+  (and (processp proc)
+       (process-get proc 'auto-pause-abort-function)))
+
+(defun auto-pause--set-process-sentinel (proc sentinel)
+  "Give PROC the sentinel SENTINEL, PROC should be an auto-pause process"
+  (when (auto-pause-process-p proc)
+    (process-sentinel proc (lambda (proc event)
+                             (funcall sentinel proc event)
+                             (when (eq 'exit (process-status proc))
+                               (funcall (process-get proc 'auto-pause-abort-function)))))))
+
+(defun auto-pause--reset-process-sentinel (proc)
+  "Reset the sentinel function of PROC which should be an auto-pause function"
+  (auto-pause--set-process-sentinel proc (process-sentinel proc)))
 
 (defun auto-pause-mark-process (proc delay-seconds)
   "Pause the PROC the next time Emacs is idle for DELAY-SECONDS, and resume the PROC when emacs become busy again"
@@ -44,14 +58,8 @@
                            (lambda ()
                              (auto-pause-resume-process proc))
                            delay-seconds))
-  (add-function :after (process-sentinel proc) (lambda (proc event)
-                                                 (when (eq 'exit (process-status proc))
-                                                   (when (process-get proc 'auto-pause-abort-function)
-                                                     (funcall (process-get proc 'auto-pause-abort-function))))))
+  (auto-pause--reset-process-sentinel proc)
   proc)
-
-(defun auto-pause-process-p (proc)
-  (process-get proc 'auto-pause-abort-function))
 
 (defmacro with-auto-pause (delay-seconds &rest body)
   "Evalute BODY, if BODY created an asynchronous subprocess, it will be auto-pause-process"
@@ -62,17 +70,14 @@
                  (lambda (proc)
                    (auto-pause-mark-process proc ,delay-seconds)))
      (advice-add 'set-process-sentinel
-                 :filter-args
+                 :after
                  (lambda (proc sentinel)
-                   (if (not (auto-pause-process-p proc))
-                       (list proc sentinel)
-                     (list proc (lambda (proc event)
-                                  (funcall sentinel proc event)
-                                  (when (eq 'exit (process-status proc))
-                                    (funcall (process-get proc 'auto-pause-abort-function))))))))
+                   (auto-pause--reset-process-sentinel proc)))
      (unwind-protect (progn ,@body)
        (advice-remove 'start-process (lambda (proc)
-                                       (auto-pause-mark-process proc ,delay-seconds))))))
+                                       (auto-pause-mark-process proc ,delay-seconds)))
+       (advice-remove 'set-process-sentinel (lambda (proc sentinel)
+                                              (auto-pause--reset-process-sentinel proc))))))
 
 ;; (defun auto-pause-make-pause-and-resume-functions (pause-fn resume-fn)
 ;;   (let (pause-function resume-function)
